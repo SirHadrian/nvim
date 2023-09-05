5fd5badfbe149fd6b6b0c3a649ac43acdb7d0ccf
Toma Adrian <sirhadrian21@gmail.com>
2023-09-06 01:10:49 +0300

    plugins update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,60 +1,77 @@
 return {
-	{
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            -- load the colorscheme here
+            vim.cmd([[colorscheme tokyonight]])
+        end,
+    },
 
-	{	
-    -- Which-key Extension
-    "folke/which-key.nvim",
-    lazy = true,
+    {
+        -- Which-key Extension
+        "folke/which-key.nvim",
+        lazy = true,
     },
     {
- -- Bufferline 
-    {
-        'akinsho/bufferline.nvim',
-	version = "*",
-        dependencies = 'nvim-tree/nvim-web-devicons'
+        -- Bufferline
+        {
+            'akinsho/bufferline.nvim',
+            version = "*",
+            dependencies = 'nvim-tree/nvim-web-devicons'
+        },
     },
-    },
     {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    }
-},
- -- Lualine 
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+        }
+    },
+    -- Lualine
     {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' }
     },
 
-     -- Alpha (Dashboard)
+    -- Alpha (Dashboard)
     {
-    'goolord/alpha-nvim',
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.dashboard'.config)
-    end
-},
+        'goolord/alpha-nvim',
+        config = function()
+            require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
+        end
+    },
 
-  -- Telescope (Fuzzy Finder)
+    -- Telescope (Fuzzy Finder)
     -- Added these plugins to install Telescope
     {
         'nvim-telescope/telescope.nvim',
         lazy = true,
         dependencies = {
-            {'nvim-lua/plenary.nvim'},
+            { 'nvim-lua/plenary.nvim' },
         }
     },
-{
-    "williamboman/mason.nvim"
-},
+    {
+        "williamboman/mason.nvim"
+    },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },           -- Required
+            { 'williamboman/mason.nvim' },         -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },   -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },   -- Required
+        }
+    },
 }
