2e61b9074149c18a4bd62126c0fcedcfb509eda4
Toma Adrian <sirhadrian21@gmail.com>
2023-09-07 01:11:31 +0300

    modules rename

--- lua/sirhadrian/options.lua
+++ lua/sirhadrian/options.lua
@@ -18,6 +18,6 @@ opt.splitright = true         -- Put new windows right of current
 opt.tabstop = 4               -- Number of spaces tabs count for
 opt.termguicolors = true      -- True color support
 opt.shell = "/usr/bin/fish"
-opt.guifont = "CaskaydiaCove Nerd Font Mono:h8"
+opt.guifont = "CaskaydiaCove Nerd Font Mono:h9"
 
 vim.g.neovide_scale_factor = 1.0
