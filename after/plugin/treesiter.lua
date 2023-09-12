20b82fe571bca75781c6967f93b32357e0ca419b
Toma Adrian <sirhadrian21@gmail.com>
2023-09-12 16:06:21 +0300

    treesiter incremental selection on tree nodes

--- after/plugin/treesiter.lua
+++ after/plugin/treesiter.lua
@@ -19,4 +19,14 @@ require 'nvim-treesitter.configs'.setup {
         enable = true,
         additional_vim_regex_highlighting = false,
     },
+
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = '<CR>',
+            scope_incremental = '<CR>',
+            node_incremental = '<TAB>',
+            node_decremental = '<S-TAB>',
+        },
+    },
 }
