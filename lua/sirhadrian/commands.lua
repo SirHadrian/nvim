29c4298c4f92446fb199b11c5f38fbd73e86533b
Toma Adrian <sirhadrian21@gmail.com>
2023-11-09 23:50:32 +0200

    update

--- lua/sirhadrian/commands.lua
+++ lua/sirhadrian/commands.lua
@@ -12,5 +12,6 @@ function Transparent()
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
   vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
   vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+  vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
 end
 Transparent()
