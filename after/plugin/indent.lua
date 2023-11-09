cbf045e95b1f09dac0d96649904ecfc8fe6ea312
Toma Adrian <sirhadrian21@gmail.com>
2023-11-09 22:11:59 +0200

    indent plugin update

--- after/plugin/indent.lua
+++ after/plugin/indent.lua
@@ -3,11 +3,4 @@ local highlight = {
     "Whitespace",
 }
 
-require("ibl").setup {
-    indent = { highlight = highlight, char = "" },
-    whitespace = {
-        highlight = highlight,
-        remove_blankline_trail = false,
-    },
-    scope = { enabled = false },
-}
+require("ibl").setup()
