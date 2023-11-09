0d24b57d93944271dada9a9c4a235b362525ccf4
Toma Adrian <sirhadrian21@gmail.com>
2023-11-09 23:39:08 +0200

    transparent popup menu

--- lua/sirhadrian/commands.lua
+++ lua/sirhadrian/commands.lua
@@ -11,5 +11,6 @@ function Transparent()
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
   vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+  vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
 end
 Transparent()
