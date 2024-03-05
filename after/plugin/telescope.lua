d096f5d1030d24949638f3656b62fb013d065df3
Toma Adrian Ionut <sirhadrian21@gmail.com>
2024-03-05 17:04:26 +0200

    format

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,27 +1,27 @@
 require("telescope").setup({
-	-- You can put your default mappings / updates / etc. in here
-	--  All the info you're looking for is in `:help telescope.setup()`
-	--
-	-- defaults = {
-	--   mappings = {
-	--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-	--   },
-	-- },
-	pickers = {
+        -- You can put your default mappings / updates / etc. in here
+        --  All the info you're looking for is in `:help telescope.setup()`
+        --
+        -- defaults = {
+        --   mappings = {
+        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+        --   },
+        -- },
+        pickers = {
 
-		-- Default configuration for builtin pickers goes here:
-		-- picker_name = {
-		--   picker_config_ key = value,
-		--   ...
-		-- }
-		-- Now the picker_config_key will be applied every time you call this
-		-- builtin picker
-	},
-	extensions = {
-		["ui-select"] = {
-			require("telescope.themes").get_dropdown(),
-		},
-	},
+                -- Default configuration for builtin pickers goes here:
+                -- picker_name = {
+                --   picker_config_ key = value,
+                --   ...
+                -- }
+                -- Now the picker_config_key will be applied every time you call this
+                -- builtin picker
+        },
+        extensions = {
+                ["ui-select"] = {
+                        require("telescope.themes").get_dropdown(),
+                },
+        },
 })
 
 -- Enable telescope extensions, if they are installed
@@ -43,23 +43,23 @@ vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find exis
 
 -- Slightly advanced example of overriding default behavior and theme
 vim.keymap.set("n", "<leader>/", function()
-	-- You can pass additional configuration to telescope to change theme, layout, etc.
-	builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-		winblend = 10,
-		previewer = false,
-	}))
+        -- You can pass additional configuration to telescope to change theme, layout, etc.
+        builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+                winblend = 10,
+                previewer = false,
+        }))
 end, { desc = "[/] Fuzzily search in current buffer" })
 
 -- Also possible to pass additional configuration options.
 --  See `:help telescope.builtin.live_grep()` for information about particular keys
 vim.keymap.set("n", "<leader>s/", function()
-	builtin.live_grep({
-		grep_open_files = true,
-		prompt_title = "Live Grep in Open Files",
-	})
+        builtin.live_grep({
+                grep_open_files = true,
+                prompt_title = "Live Grep in Open Files",
+        })
 end, { desc = "[S]earch [/] in Open Files" })
 
 -- Shortcut for searching your neovim configuration files
 vim.keymap.set("n", "<leader>sn", function()
-	builtin.find_files({ cwd = vim.fn.stdpath("config") })
+        builtin.find_files({ cwd = vim.fn.stdpath("config") })
 end, { desc = "[S]earch [N]eovim files" })
