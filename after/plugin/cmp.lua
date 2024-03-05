28ba69877c5e9adc389ad78d5d3fca6db67dec51
Toma Adrian Ionut <sirhadrian21@gmail.com>
2024-03-05 17:27:51 +0200

    cmp update

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -19,11 +19,15 @@ cmp.setup({
                 ["<C-j>"] = cmp.mapping.select_next_item(),
                 -- Select the [p]revious item
                 ["<C-k>"] = cmp.mapping.select_prev_item(),
+                -- Scroll docs
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
 
                 -- Accept ([y]es) the completion.
                 --  This will auto-import if your LSP supports it.
                 --  This will expand snippets if the LSP sent a snippet.
                 ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+                ["<CR>"] = cmp.mapping.confirm({ select = false }),
 
                 -- Manually trigger a completion from nvim-cmp.
                 --  Generally you don't need this, because nvim-cmp will display
@@ -42,16 +46,17 @@ cmp.setup({
                         if luasnip.expand_or_locally_jumpable() then
                                 luasnip.expand_or_jump()
                         end
-                        end, { "i", "s" }),
+                end, { "i", "s" }),
                 ["<C-h>"] = cmp.mapping(function()
                         if luasnip.locally_jumpable(-1) then
                                 luasnip.jump(-1)
                         end
-                        end, { "i", "s" }),
+                end, { "i", "s" }),
         }),
         sources = {
                 { name = "nvim_lsp" },
                 { name = "luasnip" },
+                { name = "buffer" },
                 { name = "path" },
         },
 })
